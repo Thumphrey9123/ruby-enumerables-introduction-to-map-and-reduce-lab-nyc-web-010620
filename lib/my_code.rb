3c304c1b430a7d190d1b2165345f016091c7c3f4
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-30 23:51:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,15 +48,14 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  arr = []
   i=0 
   while i<source_array.length do
-    if source_array[i]
-      arr << source_array[i]
+    if source_array[i] == false
+      return false
     end
     i+= 1 
   end
-  arr
+  true
 end
 
 def reduce_to_any_true(source_array)
