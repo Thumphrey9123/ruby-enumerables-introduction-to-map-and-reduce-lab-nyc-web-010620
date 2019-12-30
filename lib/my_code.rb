dc286edef364a69931b54cc70eb3b50f4bf90e5d
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-30 23:50:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,7 +63,7 @@ def reduce_to_any_true(source_array)
   i=0 
   while i<source_array.length do
     if source_array[i] == true
-      return source_array
+      return true
     end
     i+= 1 
   end
